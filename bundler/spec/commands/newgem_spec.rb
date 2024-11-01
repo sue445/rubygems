682d47ba309b3efbd6f2790a04f8bd652b285596
sue445 <sue445@sue445.net>
2024-11-02 01:57:58 +0900

    Check if `go.sum` is generated only when Go is installed
    To avoid test failure in environments where Go is not installed

--- bundler/spec/commands/newgem_spec.rb
+++ bundler/spec/commands/newgem_spec.rb
@@ -1637,7 +1637,6 @@ RSpec.describe "bundle gem" do
         expect(bundled_app("#{gem_name}/ext/#{gem_name}/#{gem_name}.h")).to exist
         expect(bundled_app("#{gem_name}/ext/#{gem_name}/extconf.rb")).to exist
         expect(bundled_app("#{gem_name}/ext/#{gem_name}/go.mod")).to exist
-        expect(bundled_app("#{gem_name}/ext/#{gem_name}/go.sum")).to exist
       end
 
       it "includes extconf.rb in gem_name.gemspec" do
@@ -1757,6 +1756,10 @@ RSpec.describe "bundle gem" do
 
         it "includes go version in go.mod" do
           expect(bundled_app("#{gem_name}/ext/#{gem_name}/go.mod").read).to include("go #{go_version}")
+        end
+
+        it "go.sum is generated" do
+          expect(bundled_app("#{gem_name}/ext/#{gem_name}/go.sum")).to exist
         end
       end
 
